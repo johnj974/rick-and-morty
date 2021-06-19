81103e902e70b10289d304d9d8bcc85a77803faa
johnj974 <jjdug@hotmail.com>
2021-06-19 11:31:31 +0100

    navbar navigation streamlined+base style to pages

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +10,17 @@ export class NavbarComponent implements OnInit {
   //.
   navbarArray: string[] = ['Home', 'Characters', 'Locations', 'Episodes'];
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  navLink(info: string) {
+    const lowerNav = info.toLowerCase();
+    console.log(lowerNav);
+    if (lowerNav === 'home') {
+      this.router.navigate(['/']);
+    } else {
+      this.router.navigate([lowerNav]);
+    }
+  }
 }
