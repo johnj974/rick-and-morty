be161cb0b7798aac35b44695479b92519180ff40
johnj974 <jjdug@hotmail.com>
2021-06-25 17:18:47 +0100

    changes to location display,retrieving resident

--- src/app/locations/search-locations/search-locations.component.ts
+++ src/app/locations/search-locations/search-locations.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LocationInterface } from 'src/app/shared/interfaces/location';
+import { CharacterService } from 'src/app/shared/services/character.service';
 import { LocationService } from 'src/app/shared/services/location.service';
 
 @Component({
@@ -9,13 +10,36 @@ import { LocationService } from 'src/app/shared/services/location.service';
 })
 export class SearchLocationsComponent implements OnInit {
   //.
+
   locationArray: LocationInterface[] = [];
-  residentsArray = [];
-  constructor(private locationService: LocationService) {}
+  retrievedResidents = [];
+
+  constructor(
+    private locationService: LocationService,
+    private characterService: CharacterService
+  ) {}
 
   ngOnInit(): void {
     this.locationService.getLocations().subscribe((data: any) => {
       this.locationArray = data.results;
     });
+  }
+
+  check(location) {
+    const residentArray = location.residents;
+    const searchArray = [];
+    for (let address of residentArray) {
+      let url = address;
+      let lastInstance = url.lastIndexOf('/');
+      let number = url.slice(lastInstance + 1);
+      searchArray.push(number);
+    }
+    console.log(searchArray);
+    this.characterService
+      .getMultipleCharacters(searchArray)
+      .subscribe((data: any) => {
+        this.retrievedResidents = data;
+        console.log(this.retrievedResidents);
+      });
   }
 }
