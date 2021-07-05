762510aa35ce84ba1c53c4882c598babb1ed1002
johnj974 <jjdug@hotmail.com>
2021-07-05 15:20:43 +0100

    single character episode display+more detail

--- src/app/characters/single-character/single-character.component.ts
+++ src/app/characters/single-character/single-character.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { CharacterInterface } from 'src/app/shared/interfaces/character';
+import { EpisodeInterface } from 'src/app/shared/interfaces/episode';
+import { CharacterService } from 'src/app/shared/services/character.service';
+import { EpisodesService } from 'src/app/shared/services/episodes.service';
 
 @Component({
   selector: 'app-single-character',
   templateUrl: './single-character.component.html',
-  styleUrls: ['./single-character.component.css']
+  styleUrls: ['./single-character.component.css'],
 })
 export class SingleCharacterComponent implements OnInit {
+  //.
 
-  constructor() { }
+  character: CharacterInterface;
+  retrievedEpisodeArray = [];
+  constructor(
+    private route: ActivatedRoute,
+    private characterService: CharacterService,
+    private episodeService: EpisodesService
+  ) {}
 
   ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.characterService
+        .getSpecificCharacter(data.id)
+        .subscribe((characterData) => {
+          this.character = characterData;
+          console.log(this.character);
+          this.retrieveEpisode();
+        });
+    });
   }
 
+  retrieveEpisode() {
+    const episodeArray = this.character.episode;
+    const searchArray = [];
+    for (let address of episodeArray) {
+      let url = address;
+      let lastInstance = url.lastIndexOf('/');
+      let number = url.slice(lastInstance + 1);
+      searchArray.push(number);
+    }
+    //console.log(searchArray);
+    this.episodeService.getEpisode(searchArray).subscribe((data: any) => {
+      this.retrievedEpisodeArray = data;
+      console.log(this.retrievedEpisodeArray);
+    });
+  }
 }
